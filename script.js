a5cabb8dd6ef1a1adc77c17157fc2896c9e34ed5
phellipe.sx <matheusphbarros2711@gmail.com>
2024-02-24 22:41:52 -0300

    add readme

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle ("light")
+    const html = document.documentElement;
+    html.classList.toggle ("light");
 
-    const img = document.querySelector("#profile img")
+    const img = document.querySelector("#profile img");
 
    if (html.classList.contains ("light")) {
 
-        img.setAtribute("src", "./assets/avatar-light.png")
+        img.setAtribute("src", "./assets/avatar-light.png");
    } else {
-        img.setAtribute("src", "./assets/avatar.png")
+        img.setAtribute("src", "./assets/avatar.png");
    }
 } 
